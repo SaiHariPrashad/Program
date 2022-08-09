7db5babdd550ddf8c2fea08be4e372d056d2b628
SaiHariPrashad <97112681+SaiHariPrashad@users.noreply.github.com>
2022-08-09 10:42:41 +0530

    Add files via upload

---
+++ Example1.cs
@@ -0,0 +1,33 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter Your Name: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Enter Your City: ");
+            string city = Console.ReadLine();
+
+            Console.Write("Enter Your Street: ");
+            string street = Console.ReadLine();
+
+            Console.Write("Enter Your House Number: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("***** Your Complete Address *****");
+            Console.WriteLine("Name = {0}", name);
+            Console.WriteLine("City = {0}", city);
+            Console.WriteLine("Street = {0}", street);
+            Console.WriteLine("House Number = {0}", num);
+            Console.ReadLine();
+        }
+    }
+}*/
